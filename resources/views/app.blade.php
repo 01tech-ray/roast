bed81d78543befae0cd871a6842f9667db2d4049
RayZhao <fhqdnnlmnm@163.com>
2019-01-03 16:43:08 +0800

    Course 1
    第一课初始化 Laravel 单页面应用

---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta  name="csrf-token" content="{{ csrf_token() }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css"/>
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type='text/javascript'>
+             window.Laravel = <?php echo json_encode([
+                'csrfToken' => csrf_token(),
+            ]); ?>
+    </script>
+</head>
+<body>
+    <div id="app">
+            <router-view></router-view>
+    </div>
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','Web\AppController@getApp')->middleWare('auth');
+
+Route::get('/','Web\AppController@getLogin');
